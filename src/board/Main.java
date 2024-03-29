d886896db2005bae0846e47f73869de8025fd083
iconew123 <iconew123@gmail.com>
2024-03-29 10:11:54 +0900

    Create Classes

--- src/board/Main.java
+++ src/board/Main.java
@@ -3,8 +3,19 @@ package board;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// 미니 콘솔 게시판 만들기
+		// Map 활용하기
+		// 상속 활용하기
+		// User CRUD활용
+		// Board CRUD활용 (글 작성자만 권한가능) , (게시판 n회이상 열람자만 글 작성 가능)
+
+		// 필요한 클래스
+		// 상속 클래스 -> 1. User , Writing : Info (id,pw)
+		//			-> 2. UserManager,WritingManager : interface(CRUD)
+		// User , Writing , UserManager(싱글톤 패턴 이용) , WritingManager(싱글톤 패턴 이용) , board-(게시글들 모음) , Main-(실행만)
+		
+		
 	}
 
 }
